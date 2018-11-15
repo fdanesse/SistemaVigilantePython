7fd4b9cca63f416ec2d5111ae89c6827bbaf4199
flavio <fdanesse@gmail.com>
2018-11-15 08:24:20 -0300

    Estructura de directorios básica

--- Vigilante/vigilante.py
+++ Vigilante/vigilante.py
@@ -15,12 +15,34 @@ from gi.repository import GstVideo
 GObject.threads_init()
 Gst.init("--opengl-hwdec-interop=vaapi-glx")
 
+VIGILANTE = os.path.join(os.environ["HOME"], "VIGILANTE")
+if not os.path.exists(VIGILANTE):
+    os.mkdir(VIGILANTE)
+
 
 class Camara(Gst.Pipeline):
-    def __init__(self):
+
+    __gsignals__ = {"estado": (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, (GObject.TYPE_STRING,))}
+
+    def __init__(self, device="/dev/video0", gtksink=None):
+
         Gst.Pipeline.__init__(self)
 
+        cameraPath = os.path.join(VIGILANTE, device.replace("/", "_"))
+        if not os.path.exists(cameraPath):
+            os.mkdir(cameraPath)
+        self.__videoPath = os.path.join(cameraPath, "Video")
+        if not os.path.exists(self.__videoPath):
+            os.mkdir(self.__videoPath)
+        self.__imagenPath = os.path.join(cameraPath, "Imagen")
+        if not os.path.exists(self.__imagenPath):
+            os.mkdir(self.__imagenPath)
+
+        self.__gtksink = gtksink
+        self.__device = device
+
         camara = Gst.ElementFactory.make('v4l2src', 'v4l2src')
+        camera.set_property("device", self.__device)
         videoconvert = Gst.ElementFactory.make('videoconvert', 'videoconvert')
         fakesink = Gst.ElementFactory.make('fakesink', 'fakesink')
 
@@ -35,16 +57,24 @@ class Camara(Gst.Pipeline):
         self.__bus.add_signal_watch()
         self.__bus.connect("message", self.__sync_message)
 
-        self.set_state(Gst.State.PLAYING)
-
         print ("Proceso Camara:", os.getpid())
 
         self.__loop = GLib.MainLoop()
-        self.__loop.run()
+        
 
     def __sync_message(self, bus, mensaje):
+        #self.emit(str(mensaje))
         pass
 
+    def play_Independiente(self):
+        self.set_state(Gst.State.PLAYING)
+        self.__loop.run()
+
+
+def Test(self, camara, info):
+    print(camara, info)
 
 if __name__=="__main__":
     camara = Camara()
+    camara.connect("estado", Test)
+    camara.play_Independiente()
